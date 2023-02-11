8e8a30a8ab857f3dec516caa569ce99a264b9b8d
gfgdjv <101093135+gfgdjv@users.noreply.github.com>
2023-02-11 15:51:32 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+const body = document.body;
+const elem = document.querySelector('#elem');
+const parent = document.querySelector('#parent');
+
+// //console.log(body.children);
+
+// //for (let node of body.children) {
+// //     console.log(node);
+// // }
+
+
+// console.log(body.childeNodes.children)
+
+// let child = body.children[1];
+
+// console.log(child.parentNode);
+
+// console.log()
+
+                            //1//
+
+elem.firstElementChild.style.color = 'red';
+
+
+                            //2//
+elem.lastElementChild.style.color = 'pink';
+
+
+                            //3//
+elem.appendChild = '!';
+
+//4//
+
+elem.closest('.elem');
+
+elem.isConnected.style.color = 'green'
